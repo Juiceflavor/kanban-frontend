1a2086f369453425bea745da4b7c902f31500502
Juiceflavor <danidag516@gmail.com>
2024-11-27 15:51:30 -0500

    ⚡️ feat(my-boards.tsx): sortablejs implemented to drag and drop boards in diferents columns states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import './assets/styles/globals.css';
 import NavBar from './components/templates/navBar';
 import Home from './views/home-page';
+import MyBoards from './views/my-boards';
 
 function MyApp() {
   return (
     <>
       <NavBar />
-      <Home />
+      <MyBoards />
     </>
   );
 }
--- src/app/views/my-boards.tsx
+++ src/app/views/my-boards.tsx
@@ -1,8 +1,10 @@
-'use client'
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+'use client';
 
-interface Task {
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import Sortable, { SortableEvent } from 'sortablejs';
+
+interface Board {
   id: number;
   title: string;
   description: string;
@@ -13,41 +15,61 @@ const states = [
   { name: 'INACTIVE', value: '000' },
   { name: 'TO_DO', value: '001' },
   { name: 'IN_PROGRESS', value: '002' },
-  { name: 'DONE', value: '003' }
+  { name: 'DONE', value: '003' },
 ];
 
 const MyBoards = () => {
-  const [taskList, setTaskList] = useState<Task[]>([]);
+  const [boardList, setBoardList] = useState<Board[]>([]);
+  const columnsRefs = useRef<Array<HTMLDivElement | null>>([]);
 
   useEffect(() => {
-    const fetchTasks = async () => {
+    const fetchBoards = async () => {
       try {
         const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/boards`);
-        setTaskList(response.data);
-        console.log('Tasks fetched:', response.data);
+        setBoardList(response.data);
       } catch (error) {
-        console.error('Error fetching tasks:', error);
+        console.error('Error fetching boards:', error);
       }
     };
 
-    fetchTasks();
+    fetchBoards();
   }, []);
+
+  useEffect(() => {
+    columnsRefs.current.forEach((column, index) => {
+      if (column) {
+        Sortable.create(column, {
+          group: 'shared',
+          animation: 150,
+        });
+      }
+    });
+  }, [boardList]);
 
   return (
     <div className="flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap space-y-4 md:space-y-0 md:space-x-4 p-4">
-      {states.map((state) => (
-        <div key={state.value} className="flex-1 md:w-1/2 lg:w-1/4 h-[790px] bg-gray-900 
-        dark:bg-gray-100 p-4 rounded overflow-hidden">
-          <h2 className="text-xl font-bold mb-4 text-white dark:text-black sticky top-0 
-          bg-gray-900 dark:bg-gray-100 p-4 rounded">
+      {states.map((state, index) => (
+        <div
+          key={state.value}
+          className="flex-1 md:w-1/2 lg:w-1/4 h-[790px] bg-gray-900 dark:bg-gray-100 p-4 rounded overflow-hidden"
+        >
+          <h2 className="text-xl font-bold mb-4 text-white dark:text-black sticky top-0 bg-gray-900 dark:bg-gray-100 p-4 rounded">
             {state.name.replace('_', ' ')}
           </h2>
-          <div className="space-y-4 overflow-y-auto h-[calc(100%-64px)]">
-            {taskList
-              .filter((task) => task.statusCode === state.value)
-              .map((task) => (
-                <div key={task.id} className="p-4 bg-gray-800 dark:bg-white rounded shadow text-white dark:text-black">
-                  {task.title}
+          <div className="space-y-4 overflow-y-auto h-[calc(100%-64px)]"
+          ref={(el) => {
+            columnsRefs.current[index] = el;
+          }}
+          >
+            {boardList
+              .filter((board) => board.statusCode === state.value)
+              .map((board) => (
+                <div
+                  key={board.id}
+                  data-id={board.id}
+                  className="p-4 bg-gray-800 dark:bg-white rounded shadow text-white dark:text-black"
+                >
+                  {board.title}
                 </div>
               ))}
           </div>
